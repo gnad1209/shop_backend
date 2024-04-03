17f597146e95bc4ea95270d13533a32923d9cf73
gnad <gnada1209@gmail.com>
2024-04-03 14:57:16 +0700

    access_token

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,51 @@ const UserService = require('../services/UserService')
 
 const createUser = async (req,res) =>{
     try{
-        console.log(req.body);
-        await UserService.createUser()
-        return res.status(200).json(res)
+        const {name,email,password,confirmPassword,phone} = req.body
+        const reg =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        const isCheckEmail = reg.test(email)
+        if(!name||!email||!password||!confirmPassword||!phone){
+            return res.status(400).json({
+                status:'err',
+                message:'the input reuired'
+            })
+        }else if(!isCheckEmail){
+            return res.status(400).json({
+                status:'err',
+                message:'the input email'
+            })
+        }else if(password !== confirmPassword){
+            return res.status(400).json({
+                status:'err',
+                message:'the input password'
+            })
+        }
+        const response = await UserService.createUser(req.body)
+        return res.status(200).json(response)
+    }catch(e){
+        return res.status(404).json({
+            message:e
+        })
+    }
+}
+const loginUser = async (req,res) =>{
+    try{
+        const {email,password,phone} = req.body
+        const reg =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        const isCheckEmail = reg.test(email)
+        if(!email||!password||!phone){
+            return res.status(400).json({
+                status:'err',
+                message:'the input reuired'
+            })
+        }else if(!isCheckEmail){
+            return res.status(400).json({
+                status:'err',
+                message:'the input email'
+            })
+        }
+        const response = await UserService.loginUser(req.body)
+        return res.status(200).json(response)
     }catch(e){
         return res.status(404).json({
             message:e
@@ -13,5 +55,6 @@ const createUser = async (req,res) =>{
 }
 
 module.exports = {
-    createUser
+    createUser,
+    loginUser
 }
