89f75630ced7b9975ea1fe5d6670c450f9d5b9a2
be' dang <144252607+gnad1209@users.noreply.github.com>
2024-06-26 10:24:52 +0700

    Update EmailService.js

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -3,17 +3,15 @@ const dotenv = require('dotenv');
 dotenv.config()
 var inlineBase64 = require('nodemailer-plugin-inline-base64');
 
-const sendEmailCreateOrder = async (email,orderItems) => {
+const sendEmailCreateOrder = async (email, orderItems) => {
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
       user: process.env.MAIL_ACCOUNT, // generated ethereal user
       pass: process.env.MAIL_PASSWORD, // generated ethereal password
     },
   });
-  transporter.use('compile', inlineBase64({cidPrefix: 'somePrefix_'}));
+  transporter.use('compile', inlineBase64({ cidPrefix: 'somePrefix_' }));
 
   let listItem = '';
   const attachImage = []
@@ -23,17 +21,23 @@ const sendEmailCreateOrder = async (email,orderItems) => {
       Bạn đã đặt sản phẩm <b>${order.name}</b> với số lượng: <b>${order.amount}</b> và giá là: <b>${order.price} VND</b></div>
       <div>Bên dưới là hình ảnh của sản phẩm</div>
     </div>`
-    attachImage.push({path: order.image})
+    attachImage.push({ path: order.image })
   })
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
+  const mailOptions = {
     from: process.env.MAIL_ACCOUNT, // sender address
-    to: email, // list of receivers
-    subject: "Bạn đã đặt hàng tại shop LẬP trình thật dễ", // Subject line
-    text: "Hello world?", // plain text body
-    html: `<div><b>Bạn đã đặt hàng thành công tại shop Lập trình thật dễ</b></div> ${listItem}`,
+    to: "ikyuss1209@gmail.com", // list of receivers
+    subject: "Bạn đã đặt hàng tại shop Đănga", // Subject line
+    html: `<div><b>Bạn đã đặt hàng thành công tại shop Đănga</b></div> ${listItem}`,
     attachments: attachImage,
+  };
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
   });
 }
 
