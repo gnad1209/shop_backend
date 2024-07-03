ac0af758ad43656baca92995729c3e7cb136df9a
be' dang <144252607+gnad1209@users.noreply.github.com>
2024-07-03 15:23:56 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 const port = process.env.PORT || 9000
 
-const io = require("socket.io")(9000,{
+const io = require("socket.io")("https://shop-gold-eight.vercel.app",{
     cors:{
         origin:"https://shop-gold-eight.vercel.app"
     }
