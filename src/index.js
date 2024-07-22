508caf721de20ec1039a88cf2e3afc2460df0081
ikyuss <danga120902@gmail.com>
2024-07-22 09:50:57 +0700

    xong

--- src/index.js
+++ src/index.js
@@ -15,12 +15,19 @@ dotenv.config()
 
 const port = process.env.PORT || 9000
 
-const io = require("socket.io")(8080, {
+// const io = require("socket.io")(8080, {
+//     cors: {
+//         origin: "http://localhost:3000"
+//     }
+// });
+const io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://shop-gold-eight.vercel.app",
+        methods: ["GET", "POST"],
+        credentials: true
+
     }
 });
-
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
