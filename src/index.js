70bbf3894f518ba696d1f4c8bf7274f33f02819d
ikyuss <danga120902@gmail.com>
2024-08-09 10:18:39 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -21,13 +21,6 @@ const io = require("socket.io")(server, {
 
 const port = process.env.PORT;
 
-// const io = require("socket.io")(8080, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   },
-// });
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,15 +1,27 @@
-const express = require("express")
-const router = express.Router()
-const ProductController = require('../controllers/ProductController')
-const { authMiddleware } = require("../middleware/authMiddleware")
+const express = require("express");
+const router = express.Router();
+const ProductController = require("../controllers/ProductController");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-router.post('/create', ProductController.createProduct)
-router.put('/update/:id',authMiddleware, ProductController.updateProduct)
-router.get('/detail/:id', ProductController.getDetailProduct)
-router.get('/getAll', ProductController.getAllProduct)
-router.delete('/delete/:id',authMiddleware, ProductController.deleteProduct)
-router.delete('/softdelete/:id',authMiddleware, ProductController.softDeleteProduct)
-router.post('/delete-many', authMiddleware, ProductController.deleteMany)
-router.get('/get-all-type', ProductController.getAllType)
+router.post("/create", upload.single("image"), ProductController.createProduct);
+router.put(
+  "/update/:id",
+  upload.single("image"),
+  authMiddleware,
+  ProductController.updateProduct
+);
+router.get("/detail/:id", ProductController.getDetailProduct);
+router.get("/getAll", ProductController.getAllProduct);
+router.delete("/delete/:id", authMiddleware, ProductController.deleteProduct);
+router.delete(
+  "/softdelete/:id",
+  authMiddleware,
+  ProductController.softDeleteProduct
+);
+router.post("/delete-many", authMiddleware, ProductController.deleteMany);
+router.get("/get-all-type", ProductController.getAllType);
 
-module.exports = router
+module.exports = router;
