c6e6b371df226bd5e28839cbdf382d424fa2538a
ikyuss <danga120902@gmail.com>
2025-01-12 12:36:20 +0700

    update-code

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -1,32 +1,32 @@
-const nodemailer = require('nodemailer')
-const dotenv = require('dotenv');
-dotenv.config()
-var inlineBase64 = require('nodemailer-plugin-inline-base64');
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
+var inlineBase64 = require("nodemailer-plugin-inline-base64");
 
 const sendEmailCreateOrder = async (email, orderItems) => {
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.MAIL_ACCOUNT, // generated ethereal user
       pass: process.env.MAIL_PASSWORD, // generated ethereal password
     },
   });
-  transporter.use('compile', inlineBase64({ cidPrefix: 'somePrefix_' }));
+  transporter.use("compile", inlineBase64({ cidPrefix: "somePrefix_" }));
 
-  let listItem = '';
-  const attachImage = []
+  let listItem = "";
+  const attachImage = [];
   orderItems.forEach((order) => {
     listItem += `<div>
     <div>
       Bạn đã đặt sản phẩm <b>${order.name}</b> với số lượng: <b>${order.amount}</b> và giá là: <b>${order.price} VND</b></div>
       <div>Bên dưới là hình ảnh của sản phẩm</div>
-    </div>`
-    attachImage.push({ path: order.image })
-  })
+    </div>`;
+    attachImage.push({ path: order.image });
+  });
 
   const mailOptions = {
     from: process.env.MAIL_ACCOUNT, // sender address
-    to: "ikyuss1209@gmail.com", // list of receivers
+    to: email, // list of receivers
     subject: "Bạn đã đặt hàng tại shop Đănga", // Subject line
     html: `<div><b>Bạn đã đặt hàng thành công tại shop Đănga</b></div> ${listItem}`,
     attachments: attachImage,
@@ -36,11 +36,11 @@ const sendEmailCreateOrder = async (email, orderItems) => {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Email sent: " + info.response);
     }
   });
-}
+};
 
 module.exports = {
-  sendEmailCreateOrder
-}
+  sendEmailCreateOrder,
+};
--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,83 +1,98 @@
-const Order = require("../models/OderProduct")
-const Product = require("../models/ProductModel")
-const EmailService = require("../services/EmailService")
+const Order = require("../models/OderProduct");
+const Product = require("../models/ProductModel");
+const EmailService = require("../services/EmailService");
 
 const createOrder = (newOrder) => {
-    return new Promise(async (resolve, reject) => {
-        const { orderItems, paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone, user, isPaid, paidAt, email } = newOrder
-        try {
-            const promises = orderItems.map(async (order) => {
-                const productData = await Product.findOneAndUpdate(
-                    {
-                        _id: order.product,
-                        countInStock: { $gte: order.amount }
-                    },
-                    {
-                        $inc: {
-                            countInStock: -order.amount,
-                            selled: +order.amount
-                        }
-                    },
-                    { new: true }
-                )
-                if (productData) {
-                    return {
-                        status: 'OK',
-                        message: 'SUCCESS'
-                    }
-                }
-                else {
-                    return {
-                        status: 'OK',
-                        message: 'ERR',
-                        id: order.product
-                    }
-                }
-            })
-            const results = await Promise.all(promises)
-            const newData = results && results.filter((item) => item.id)
-            if (newData.length) {
-                const arrId = []
-                newData.forEach((item) => {
-                    arrId.push(item.id)
-                })
-                resolve({
-                    status: 'ERR',
-                    message: `San pham voi id: ${arrId.join(',')} khong du hang`
-                })
-            } else {
-                const createdOrder = await Order.create({
-                    orderItems,
-                    shippingAddress: {
-                        fullName,
-                        address,
-                        city, phone
-                    },
-                    paymentMethod,
-                    itemsPrice,
-                    shippingPrice,
-                    totalPrice,
-                    user: user,
-                    isPaid, paidAt
-                })
-                resolve({
-                    status: 'OK',
-                    message: 'success'
-                })
-                if (createdOrder) {
-                    await EmailService.sendEmailCreateOrder(email, orderItems)
-                    resolve({
-                        status: 'OK',
-                        message: 'success'
-                    })
-                }
-            }
-        } catch (e) {
-            //   console.log('e', e)
-            reject(e)
+  return new Promise(async (resolve, reject) => {
+    const {
+      orderItems,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      totalPrice,
+      fullName,
+      address,
+      city,
+      phone,
+      user,
+      isPaid,
+      paidAt,
+      email,
+    } = newOrder;
+    try {
+      const promises = orderItems.map(async (order) => {
+        const productData = await Product.findOneAndUpdate(
+          {
+            _id: order.product,
+            countInStock: { $gte: order.amount },
+          },
+          {
+            $inc: {
+              countInStock: -order.amount,
+              selled: +order.amount,
+            },
+          },
+          { new: true }
+        );
+        if (productData) {
+          return {
+            status: "OK",
+            message: "SUCCESS",
+          };
+        } else {
+          return {
+            status: "OK",
+            message: "ERR",
+            id: order.product,
+          };
         }
-    })
-}
+      });
+      const results = await Promise.all(promises);
+      const newData = results && results.filter((item) => item.id);
+      if (newData.length) {
+        const arrId = [];
+        newData.forEach((item) => {
+          arrId.push(item.id);
+        });
+        resolve({
+          status: "ERR",
+          message: `San pham voi id: ${arrId.join(",")} khong du hang`,
+        });
+      } else {
+        const createdOrder = await Order.create({
+          orderItems,
+          shippingAddress: {
+            fullName,
+            address,
+            city,
+            phone,
+          },
+          paymentMethod,
+          itemsPrice,
+          shippingPrice,
+          totalPrice,
+          user: user,
+          isPaid,
+          paidAt,
+        });
+        resolve({
+          status: "OK",
+          message: "success",
+        });
+        if (createdOrder) {
+          await EmailService.sendEmailCreateOrder(email, orderItems);
+          resolve({
+            status: "OK",
+            message: "success",
+          });
+        }
+      }
+    } catch (e) {
+      //   console.log('e', e)
+      reject(e);
+    }
+  });
+};
 
 // const deleteManyProduct = (ids) => {
 //     return new Promise(async (resolve, reject) => {
@@ -94,129 +109,132 @@ const createOrder = (newOrder) => {
 // }
 
 const getAllOrderDetails = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const order = await Order.find({
-                user: id, isDelete: false
-            }).sort({ createdAt: -1, updatedAt: -1 })
-            if (order === null) {
-                resolve({
-                    status: 'ERR',
-                    message: 'The order is not defined'
-                })
-            }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const order = await Order.find({
+        user: id,
+        isDelete: false,
+      }).sort({ createdAt: -1, updatedAt: -1 });
+      if (order === null) {
+        resolve({
+          status: "ERR",
+          message: "The order is not defined",
+        });
+      }
 
-            resolve({
-                status: 'OK',
-                message: 'SUCESSS',
-                data: order
-            })
-        } catch (e) {
-            // console.log('e', e)
-            reject(e)
-        }
-    })
-}
+      resolve({
+        status: "OK",
+        message: "SUCESSS",
+        data: order,
+      });
+    } catch (e) {
+      // console.log('e', e)
+      reject(e);
+    }
+  });
+};
 
 const getOrderDetails = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const order = await Order.findById({
-                _id: id,
-                isDelete: false
-            })
-            if (order === null) {
-                resolve({
-                    status: 'ERR',
-                    message: 'The order is not defined'
-                })
-            }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const order = await Order.findById({
+        _id: id,
+        isDelete: false,
+      });
+      if (order === null) {
+        resolve({
+          status: "ERR",
+          message: "The order is not defined",
+        });
+      }
 
-            resolve({
-                status: 'OK',
-                message: 'SUCESSS',
-                data: order
-            })
-        } catch (e) {
-            // console.log('e', e)
-            reject(e)
-        }
-    })
-}
+      resolve({
+        status: "OK",
+        message: "SUCESSS",
+        data: order,
+      });
+    } catch (e) {
+      // console.log('e', e)
+      reject(e);
+    }
+  });
+};
 
 const cancelOrderDetails = (id, data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let order = []
-            const promises = data.map(async (order) => {
-                const productData = await Product.findOneAndUpdate(
-                    {
-                        _id: order.product,
-                        selled: { $gte: order.amount }
-                    },
-                    {
-                        $inc: {
-                            countInStock: +order.amount,
-                            selled: -order.amount
-                        }
-                    },
-                    { new: true }
-                )
-                if (productData) {
-                    order = await Order.findByIdAndUpdate(id, { isDelete: true })
-                    if (order === null) {
-                        resolve({
-                            status: 'ERR',
-                            message: 'The order is not defined'
-                        })
-                    }
-                } else {
-                    return {
-                        status: 'OK',
-                        message: 'ERR',
-                        id: order.product
-                    }
-                }
-            })
-            const results = await Promise.all(promises)
-            const newData = results && results[0] && results[0].id
-
-            if (newData) {
-                resolve({
-                    status: 'ERR',
-                    message: `San pham voi id: ${newData} khong ton tai`
-                })
-            }
+  return new Promise(async (resolve, reject) => {
+    try {
+      let order = [];
+      const promises = data.map(async (order) => {
+        const productData = await Product.findOneAndUpdate(
+          {
+            _id: order.product,
+            selled: { $gte: order.amount },
+          },
+          {
+            $inc: {
+              countInStock: +order.amount,
+              selled: -order.amount,
+            },
+          },
+          { new: true }
+        );
+        if (productData) {
+          order = await Order.findByIdAndUpdate(id, { isDelete: true });
+          if (order === null) {
             resolve({
-                status: 'OK',
-                message: 'success',
-                data: order
-            })
-        } catch (e) {
-            reject(e)
+              status: "ERR",
+              message: "The order is not defined",
+            });
+          }
+        } else {
+          return {
+            status: "OK",
+            message: "ERR",
+            id: order.product,
+          };
         }
-    })
-}
+      });
+      const results = await Promise.all(promises);
+      const newData = results && results[0] && results[0].id;
+      if (newData) {
+        resolve({
+          status: "ERR",
+          message: `San pham voi id: ${newData} khong ton tai`,
+        });
+      }
+      resolve({
+        status: "OK",
+        message: "success",
+        data: order,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 const getAllOrder = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const allOrder = await Order.find({ isDelete: false }).sort({ createdAt: -1, updatedAt: -1 })
-            resolve({
-                status: 'OK',
-                message: 'Success',
-                data: allOrder
-            })
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      const allOrder = await Order.find({ isDelete: false }).sort({
+        createdAt: -1,
+        updatedAt: -1,
+      });
+      resolve({
+        status: "OK",
+        message: "Success",
+        data: allOrder,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
 
 module.exports = {
-    createOrder,
-    getAllOrderDetails,
-    getOrderDetails,
-    cancelOrderDetails,
-    getAllOrder
-}
+  createOrder,
+  getAllOrderDetails,
+  getOrderDetails,
+  cancelOrderDetails,
+  getAllOrder,
+};
